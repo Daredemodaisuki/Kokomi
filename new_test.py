8fa79985a1762b1abbb89ef982348d77e6c97347
Daredemodaisuki <64051837+Daredemodaisuki@users.noreply.github.com>
2024-01-14 14:52:27 +0800

    更新测试样例

--- new_test.py
+++ new_test.py
@@ -2,9 +2,7 @@ from main import Kokomi
 from main import OceanHuedClam
 
 # 检索同时在上京站和人民广场周围1000m内的公交站
-waifu = Kokomi()
-waifu.Watatsumi_set("OGF")
-Q3 = OceanHuedClam("node")\
+Q0 = OceanHuedClam("node")\
     .include_OceanHuedClam("SJZstop", OceanHuedClam("node")\
                                      .include_OceanHuedClam("SJZ", OceanHuedClam("node")\
                                                                   .id(301074167))\
@@ -17,59 +15,28 @@ Q3 = OceanHuedClam("node")\
                                       .around("RMGC", 1000))\
     .set_from(["SJZstop", "RMGCstop"])
 
-waifu.query("data=[out:xml][timeout:500];" + Q3.convert() + "out body;")
-print(waifu.directive_text_temp)
-
-'''
-Q1 = OceanHuedClam("node") \
-    .include_OceanHuedClam("SJZ", OceanHuedClam("node").id(301074167)) \
-    .key_value("highway", "=", "bus_stop") \
-    .around("SJZ", 1000)
-Q2 = OceanHuedClam("node") \
-    .include_OceanHuedClam("RMGC", OceanHuedClam("way").id(28800254)) \
-    .key_value("highway", "=", "bus_stop") \
-    .around("RMGC", 1000)
-Q3 = OceanHuedClam("node") \
-    .include_OceanHuedClam("SJZstop", Q1) \
-    .include_OceanHuedClam("RMGCstop", Q2) \
-    .set_from(["SJZstop", "RMGCstop"])
 waifu = Kokomi()
 waifu.Watatsumi_set("OGF")
-waifu.query("data=[out:xml][timeout:500];" + Q3.convert() + "out body;")
+waifu.query(Q0)
 print(waifu.directive_text_temp)
-'''
-# print(Q3.convert())
 
-# 检验id是否可以同kv一并使用 -> 不可以
-'''Q4 = OceanHuedClam("node")\
-    .id(301074167)\
-    .key_value("highway", "exist")
+# 检索在上京站或人民广场周围1000m内的公交站
+Q1 = OceanHuedClam("node")\
+    .include_OceanHuedClam("SJZstop", OceanHuedClam("node")\
+                                     .include_OceanHuedClam("SJZ", OceanHuedClam("node")\
+                                                                  .id(301074167))\
+                                     .key_value("highway", "=", "bus_stop")
+                                     .around("SJZ", 1000))\
+    .include_OceanHuedClam("RMGCstop", OceanHuedClam("node")\
+                                      .include_OceanHuedClam("RMGC", OceanHuedClam("way")\
+                                                                    .id(28800254))\
+                                      .key_value("highway", "=", "bus_stop")\
+                                      .around("RMGC", 1000))\
+    .set_from("SJZstop").set_from("RMGCstop").key_value("name", "exist")
 
 waifu1 = Kokomi()
 waifu1.Watatsumi_set("OGF")
-waifu1.query("data=[out:xml][timeout:500];" + Q4.convert() + "out body;")
-print(waifu1.directive_text_temp)'''
-
-# 检索在上京站或人民广场周围1000m内的公交站
-waifu = Kokomi()
-waifu.Watatsumi_set("OGF")
-
-Q3 = OceanHuedClam("node")\
-    .include_OceanHuedClam("SJZstop", OceanHuedClam("node")\
-                                     .include_OceanHuedClam("SJZ", OceanHuedClam("node")\
-                                                                  .id(301074167))\
-                                     .key_value("highway", "=", "bus_stop")
-                                     .around("SJZ", 1000))\
-    .include_OceanHuedClam("RMGCstop", OceanHuedClam("node")\
-                                      .include_OceanHuedClam("RMGC", OceanHuedClam("way")\
-                                                                    .id(28800254))\
-                                      .key_value("highway", "=", "bus_stop")\
-                                      .around("RMGC", 1000))\
-    .set_from("SJZstop").set_from("RMGCstop").set_from(["SJZstop", "RMGCstop"])
-
-Q_list = Q3.how_many_query()
-for x in Q_list:
-    # TODO:合并还没做；how_many_query()之后并入query函数
-    waifu.query("data=[out:xml][timeout:500];" + x.convert() + "out body;")
-
-    print(waifu.directive_text_temp)
+waifu1.query(Q1)
+print(len(waifu1.directive_dict["node"]))  # -> 30 + 31 - 1交集 = 60成功
+# print(waifu1.directive_text_temp)
+# TODO:Kokomi中query之后的合并 -> 原来已经合并了啊

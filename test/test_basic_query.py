22fbc6b4fdf34aab3bc0ff2c3a7a1d8377dce497
Daredemodaisuki <64051837+Daredemodaisuki@users.noreply.github.com>
2024-01-14 20:06:00 +0800

    更新测试样例

--- test/test_basic_query.py
+++ test/test_basic_query.py
@@ -4,47 +4,65 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
 from Kokomi.main import Kokomi, OceanHuedClam
 
-# test
-# 测试区
-"""
-waifu = Kokomi()
-waifu.Watatsumi_set("OGF")
-waifu.query('data=[out:xml][timeout:25];relation["name"="华夏共和国陆地"];out body;')
-# "data=[out:xml][timeout:25];node[name](id:6229630725);out meta;>;out meta qt;"
-# "data=[out:xml][timeout:25];(area[\"ISO3166-1\"=\"GB\"][admin_level=2]; )->.a;"
-print("源报文：", waifu.directive_text_temp)
-print("============分割线============")
-for test_data in waifu.directive_dict["relation"]:
-    print(
-        "测试数据ID：" + test_data,
-        "\n",
-        waifu.directive_dict["relation"][test_data],
-    )
-waifu.how_are_you()
-waifu.I_come_back()
-waifu.how_are_you()
-"""
+# 基本测试：是否跑得通，带多层集合的情况下
+Q1 = OceanHuedClam("nwr")\
+    .key_value("name", "v-reg", "郁")  # 限制语句1：nwr["name"~"郁"]
+Q2 = OceanHuedClam("node").include_OceanHuedClam("a", Q1).set_from("a")\
+    .key_value("railway", "exist")  # ↑将限制1作为集合a的条件：引用集合set1，并设定为在set1中检索限制语句2：nwr["railway"]
+Q3 = OceanHuedClam("node").include_OceanHuedClam("b", Q2).set_from("b")\
+    .key_value("public_transport", "exist").key_value("train", "=", "yes")  # 同理再次套用，得到最终语句
+
+for x in Q3.convert():
+    print("最终语句为" + x)
 
 waifu = Kokomi()
 waifu.Watatsumi_set("OGF")
-limit = OceanHuedClam("nwr").timeout("25").key_value("name", "v-reg", "郁州")
-waifu.query(
-    'data=[out:xml][timeout:100];(nwr["name"~"郁州"];)->.a;(nwr.a["railway"];);out body;'
-)
-print(waifu.directive_text_temp)
-"""
-for test_data in waifu.directive_dict["node"]:
-    print("点ID：" + test_data, "：", waifu.directive_dict["node"][test_data])
-for test_data in waifu.directive_dict["relation"]:
-    print(
-        "关系ID：" + test_data, "：", waifu.directive_dict["relation"][test_data]
-    )
+waifu.query(Q3)
 
-waifu.directive_dict["node"] = {}
-waifu.directive_dict["relation"] = {}
+'''
+【成功】
+测试输出（数据截至2024-01-14）
+————————————————————————————————————————
+INFO: OceanHuedClam a is included.
+信息：「海染砗磲」“a”已装备。
 
-limit = limit.key_value("railway", "exist")
-waifu.query(limit.get_this_text())
-for test_data in waifu.directive_dict["node"]:
-    print("点ID：" + test_data, "：", waifu.directive_dict["node"][test_data])
-"""
+INFO: OceanHuedClam b is included.
+信息：「海染砗磲」“b”已装备。
+
+最终语句为nwr["name"~"郁"]->.a;node.a["railway"]->.b;node.b["public_transport"]["train"="yes"];
+INFO: Kokomi is ready.
+Kokomi就绪。
+
+INFO: Define Sangonomiya(Overpass) for Watatsumi(Network) successfully.
+信息：已在海祇岛上建立珊瑚宫。（指定Overpass成功）：
+ OGF https://overpass.ogf.rent-a-planet.com/api/ 
+
+INFO: Kokomi is finding in Sangonomiya(Overpass) ( 1 / 1 ):
+信息：Kokomi正在珊瑚宫（Overpass）翻箱倒柜（第 1 次/共 1 次）：
+https://overpass.ogf.rent-a-planet.com/api/interpreter?data=[out:xml][timeout:500];nwr["name"~"郁"]->.a;node.a["railway"]->.b;node.b["public_transport"]["train"="yes"];out body; 
+
+INFO: 3 node (s) have been found.
+信息：Kokomi找到了 3 个 node 。
+
+INFO: 0 way (s) have been found.
+信息：Kokomi找到了 0 个 way 。
+
+INFO: 0 relation (s) have been found.
+信息：Kokomi找到了 0 个 relation 。
+'''
+
+'''
+老版本文段存档（没有联网查询）
+————————————————————————————————————————
+limit1 = OceanHuedClam("nwr", 0).key_value("name", "v-reg", "郁")  # 限制语句1：nwr["name"~"郁"]
+set1 = Jellyfish("a", limit1)  # 将限制1作为集合a的条件
+limit2 = OceanHuedClam("node", 0).import_jellyfish(set1).in_jellyfish("a").key_value("railway", "exist")
+# 引用集合set1，并设定为在set1中检索限制语句2：nwr["railway"]
+set2 = Jellyfish("b", limit2)
+limit3 = OceanHuedClam("nwr").import_jellyfish(set2).in_jellyfish("b").key_value("public_transport", "exist")\
+    .key_value("train", "=", "yes").timeout("100")
+
+print("最终语句为" + limit3.get_full_text())  # 得到最终语句
+'''
+
+
--- test/test_radius_query.py
+++ test/test_radius_query.py
@@ -7,92 +7,167 @@ from Kokomi.main import Kokomi, OceanHuedClam
 # 检索同时在上京站和人民广场周围1000m内的公交站
 waifu = Kokomi()
 waifu.Watatsumi_set("OGF")
-Q3 = (
-    OceanHuedClam("node")
-    .include_OceanHuedClam(
-        "SJZstop",
-        OceanHuedClam("node")
-        .include_OceanHuedClam("SJZ", OceanHuedClam("node").id(301074167))
-        .key_value("highway", "=", "bus_stop")
-        .around("SJZ", 1000),
-    )
-    .include_OceanHuedClam(
-        "RMGCstop",
-        OceanHuedClam("node")
-        .include_OceanHuedClam("RMGC", OceanHuedClam("way").id(28800254))
-        .key_value("highway", "=", "bus_stop")
-        .around("RMGC", 1000),
-    )
+Q3 = OceanHuedClam("node")\
+    .include_OceanHuedClam("SJZstop", OceanHuedClam("node")\
+                                     .include_OceanHuedClam("SJZ", OceanHuedClam("node")\
+                                                                  .id(301074167))\
+                                     .key_value("highway", "=", "bus_stop")
+                                     .around("SJZ", 1000))\
+    .include_OceanHuedClam("RMGCstop", OceanHuedClam("node")\
+                                      .include_OceanHuedClam("RMGC", OceanHuedClam("way")\
+                                                                    .id(28800254))\
+                                      .key_value("highway", "=", "bus_stop")\
+                                      .around("RMGC", 1000))\
     .set_from(["SJZstop", "RMGCstop"])
-)
 
-waifu.query("data=[out:xml][timeout:500];" + Q3.convert() + "out body;")
+waifu.query(Q3)
 print(waifu.directive_text_temp)
 
-"""
-Q1 = (
-    OceanHuedClam("node")
-    .include_OceanHuedClam("SJZ", OceanHuedClam("node").id(301074167))
-    .key_value("highway", "=", "bus_stop")
-    .around("SJZ", 1000)
-)
-Q2 = (
-    OceanHuedClam("node")
-    .include_OceanHuedClam("RMGC", OceanHuedClam("way").id(28800254))
-    .key_value("highway", "=", "bus_stop")
-    .around("RMGC", 1000)
-)
-Q3 = (
-    OceanHuedClam("node")
-    .include_OceanHuedClam("SJZstop", Q1)
-    .include_OceanHuedClam("RMGCstop", Q2)
-    .set_from(["SJZstop", "RMGCstop"])
-)
-waifu = Kokomi()
-waifu.Watatsumi_set("OGF")
-waifu.query("data=[out:xml][timeout:500];" + Q3.convert() + "out body;")
-print(waifu.directive_text_temp)
-"""
-# print(Q3.convert())
+'''
+【成功】
+测试输出（数据截至2024-01-14）
+————————————————————————————————————————
+INFO: Kokomi is ready.
+Kokomi就绪。
 
-# 检验id是否可以同kv一并使用 -> 不可以
-"""
-Q4 = OceanHuedClam("node").id(301074167).key_value("highway", "exist")
+INFO: Define Sangonomiya(Overpass) for Watatsumi(Network) successfully.
+信息：已在海祇岛上建立珊瑚宫。（指定Overpass成功）：
+ OGF https://overpass.ogf.rent-a-planet.com/api/ 
 
-waifu1 = Kokomi()
-waifu1.Watatsumi_set("OGF")
-waifu1.query("data=[out:xml][timeout:500];" + Q4.convert() + "out body;")
-print(waifu1.directive_text_temp)
-"""
+INFO: OceanHuedClam SJZ is included.
+信息：「海染砗磲」“SJZ”已装备。
+
+INFO: OceanHuedClam SJZstop is included.
+信息：「海染砗磲」“SJZstop”已装备。
+
+INFO: OceanHuedClam RMGC is included.
+信息：「海染砗磲」“RMGC”已装备。
+
+INFO: OceanHuedClam RMGCstop is included.
+信息：「海染砗磲」“RMGCstop”已装备。
+
+WARN: When there is id limitation in a OceanHuedClam, other limitations cannot function.
+警示意义的：使用id限定后，其他「海染砗磲」条件无法生效。
+
+WARN: When there is id limitation in a OceanHuedClam, other limitations cannot function.
+警示意义的：使用id限定后，其他「海染砗磲」条件无法生效。
+
+INFO: OceanHuedClam default uses data from a intersection of multiple OceanHuedClams.
+信息：所装备的「海染砗磲」“全集”使用了多个其他「海染砗磲」的交集。
+
+INFO: Kokomi is finding in Sangonomiya(Overpass) ( 1 / 1 ):
+信息：Kokomi正在珊瑚宫（Overpass）翻箱倒柜（第 1 次/共 1 次）：
+https://overpass.ogf.rent-a-planet.com/api/interpreter?data=[out:xml][timeout:500];node(id:301074167)->.SJZ;node(around.SJZ:1000)["highway"="bus_stop"]->.SJZstop;way(id:28800254)->.RMGC;node(around.RMGC:1000)["highway"="bus_stop"]->.RMGCstop;node.SJZstop.RMGCstop;out body; 
+
+INFO: 1 node (s) have been found.
+信息：Kokomi找到了 1 个 node 。
+
+INFO: 0 way (s) have been found.
+信息：Kokomi找到了 0 个 way 。
+
+INFO: 0 relation (s) have been found.
+信息：Kokomi找到了 0 个 relation 。
+
+<?xml version="1.0" encoding="UTF-8"?>
+<osm version="0.6" generator="Overpass API 0.7.58.2 2b5354b1">
+<note>The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.</note>
+<meta osm_base="2024-01-14T11:51:51Z"/>
+
+  <node id="335187564" lat="12.4847682" lon="148.4085599">
+    <tag k="bus" v="yes"/>
+    <tag k="highway" v="bus_stop"/>
+    <tag k="name" v="民安坊公园"/>
+    <tag k="public_transport" v="platform"/>
+  </node>
+
+</osm>
+'''
 
 # 检索在上京站或人民广场周围1000m内的公交站
 waifu = Kokomi()
 waifu.Watatsumi_set("OGF")
 
-Q3 = (
-    OceanHuedClam("node")
-    .include_OceanHuedClam(
-        "SJZstop",
-        OceanHuedClam("node")
-        .include_OceanHuedClam("SJZ", OceanHuedClam("node").id(301074167))
-        .key_value("highway", "=", "bus_stop")
-        .around("SJZ", 1000),
-    )
-    .include_OceanHuedClam(
-        "RMGCstop",
-        OceanHuedClam("node")
-        .include_OceanHuedClam("RMGC", OceanHuedClam("way").id(28800254))
-        .key_value("highway", "=", "bus_stop")
-        .around("RMGC", 1000),
-    )
-    .set_from("SJZstop")
-    .set_from("RMGCstop")
-    .set_from(["SJZstop", "RMGCstop"])
-)
+Q3 = OceanHuedClam("node")\
+    .include_OceanHuedClam("SJZstop", OceanHuedClam("node")\
+                                     .include_OceanHuedClam("SJZ", OceanHuedClam("node")\
+                                                                  .id(301074167))\
+                                     .key_value("highway", "=", "bus_stop")
+                                     .around("SJZ", 1000))\
+    .include_OceanHuedClam("RMGCstop", OceanHuedClam("node")\
+                                      .include_OceanHuedClam("RMGC", OceanHuedClam("way")\
+                                                                    .id(28800254))\
+                                      .key_value("highway", "=", "bus_stop")\
+                                      .around("RMGC", 1000))\
+    .set_from("SJZstop").set_from("RMGCstop").key_value("name", "exist")
 
-Q_list = Q3.how_many_query()
-for x in Q_list:
-    # TODO:合并还没做；how_many_query()之后并入query函数
-    waifu.query("data=[out:xml][timeout:500];" + x.convert() + "out body;")
+waifu1 = Kokomi()
+waifu1.Watatsumi_set("OGF")
+waifu1.query(Q3)
+print(len(waifu1.directive_dict["node"]))  # -> 30 + 31 - 1交集 = 60成功
+# print(waifu1.directive_text_temp)
+# TODO:Kokomi中query之后的合并 -> 原来已经合并了啊
 
-    print(waifu.directive_text_temp)
+'''
+【成功】
+测试输出（数据截至2024-01-14）
+————————————————————————————————————————
+INFO: Kokomi is ready.
+Kokomi就绪。
+
+INFO: Define Sangonomiya(Overpass) for Watatsumi(Network) successfully.
+信息：已在海祇岛上建立珊瑚宫。（指定Overpass成功）：
+ OGF https://overpass.ogf.rent-a-planet.com/api/ 
+
+INFO: OceanHuedClam SJZ is included.
+信息：「海染砗磲」“SJZ”已装备。
+
+INFO: OceanHuedClam SJZstop is included.
+信息：「海染砗磲」“SJZstop”已装备。
+
+INFO: OceanHuedClam RMGC is included.
+信息：「海染砗磲」“RMGC”已装备。
+
+INFO: OceanHuedClam RMGCstop is included.
+信息：「海染砗磲」“RMGCstop”已装备。
+
+INFO: Kokomi is ready.
+Kokomi就绪。
+
+INFO: Define Sangonomiya(Overpass) for Watatsumi(Network) successfully.
+信息：已在海祇岛上建立珊瑚宫。（指定Overpass成功）：
+ OGF https://overpass.ogf.rent-a-planet.com/api/ 
+
+WARN: When there is id limitation in a OceanHuedClam, other limitations cannot function.
+警示意义的：使用id限定后，其他「海染砗磲」条件无法生效。
+
+WARN: When there is id limitation in a OceanHuedClam, other limitations cannot function.
+警示意义的：使用id限定后，其他「海染砗磲」条件无法生效。
+
+INFO: Kokomi is finding in Sangonomiya(Overpass) ( 1 / 2 ):
+信息：Kokomi正在珊瑚宫（Overpass）翻箱倒柜（第 1 次/共 2 次）：
+https://overpass.ogf.rent-a-planet.com/api/interpreter?data=[out:xml][timeout:500];node(id:301074167)->.SJZ;node(around.SJZ:1000)["highway"="bus_stop"]->.SJZstop;way(id:28800254)->.RMGC;node(around.RMGC:1000)["highway"="bus_stop"]->.RMGCstop;node.SJZstop["name"];out body; 
+
+INFO: 30 node (s) have been found.
+信息：Kokomi找到了 30 个 node 。
+
+INFO: 0 way (s) have been found.
+信息：Kokomi找到了 0 个 way 。
+
+INFO: 0 relation (s) have been found.
+信息：Kokomi找到了 0 个 relation 。
+
+INFO: Kokomi is finding in Sangonomiya(Overpass) ( 2 / 2 ):
+信息：Kokomi正在珊瑚宫（Overpass）翻箱倒柜（第 2 次/共 2 次）：
+https://overpass.ogf.rent-a-planet.com/api/interpreter?data=[out:xml][timeout:500];node(id:301074167)->.SJZ;node(around.SJZ:1000)["highway"="bus_stop"]->.SJZstop;way(id:28800254)->.RMGC;node(around.RMGC:1000)["highway"="bus_stop"]->.RMGCstop;node.RMGCstop["name"];out body; 
+
+INFO: 31 node (s) have been found.
+信息：Kokomi找到了 31 个 node 。
+
+INFO: 0 way (s) have been found.
+信息：Kokomi找到了 0 个 way 。
+
+INFO: 0 relation (s) have been found.
+信息：Kokomi找到了 0 个 relation 。
+
+60
+'''
